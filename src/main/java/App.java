b16d6a1184d498679883abb8fa1c936a4f09ee98
IStep777 <zoomzoom174175@gmail.com>
2023-11-16 08:14:33 +0300

    v2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,10 +5,16 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld beanHelloWorld1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld beanHelloWorld2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(beanHelloWorld1.getMessage());
 
-        Cat beanCat = (Cat) applicationContext.getBean("cat");
+        Cat beanCat1 = (Cat) applicationContext.getBean("cat");
+        Cat beanCat2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println("Сравнение бинов helloWorld: " + (beanHelloWorld1==beanHelloWorld2));
+        System.out.println("Сравнение бинов Cat: " + (beanCat1==beanCat2));
     }
 }
